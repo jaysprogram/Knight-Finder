0f6d498ce1bba97f4fdabddcfa0b1f6594fbeb08
SliferGod <jacob.w.salmon@gmail.com>
2025-01-31 20:34:27 -0500

    Added some code for saving and getting saved data.

--- popup.js
+++ popup.js
@@ -4,10 +4,57 @@ function preventRefresh(e){
   // e.preventDefault stops the page from refreshing after input
   if (e.preventDefault) e.preventDefault();
 }
+
+async function saveHistory(searchRequest) {
+  /*
+  return new Promise((resolve, reject) => {
+
+  })
+
+  try {
+
+  }
+  catch(error) {
+    console.error(error);
+  }
+  
+  // In-page cache of the user's options
+  const options = {};
+  const optionsForm = document.getElementById("optionsForm");
+  
+  // Immediately persist options changes
+  optionsForm.debug.addEventListener("change", (event) => {
+    options.debug = event.target.checked;
+    chrome.storage.sync.set({ options });
+  });
+  
+  // Initialize the form with the user's option settings
+  const data = await chrome.storage.sync.get("options");
+  Object.assign(options, data.options);
+  optionsForm.debug.checked = Boolean(options.debug);
+
+  */
+
+    let pastSearches = {};
+    pastSearches.history1 = searchRequest;
+
+    chrome.storage.sync.set({pastSearches});
+}
+
+async function loadHistory() {
+  const data = await chrome.storage.sync.get("pastSearches");
+  const pastSearches = {};
+  Object.assign(pastSearches, data.pastSearches);
+  history.innerHTML = (String)(pastSearches.history1);
+}
+
 function processSearchBox(e){
   // Saving a duplicate the user enters and reset original to empty
   let searchRequest = searchBoxElement.value;
   searchBoxElement.value = "";
+
+  //Save the history
+  saveHistory(searchRequest);
 
   // call preventRefresh
   preventRefresh(e);
@@ -20,4 +67,10 @@ let searchBoxElement = document.getElementById("searchBar");
 
 // Runs the function processSearchBox
 searchBoxForm.addEventListener("submit",processSearchBox);
+
+
+
+//Call the asynchronous function to set the history
+let history = document.getElementById("history");
+loadHistory();
 
