b7c0b85b240511d0de118aa0aca0fbb7d0b823fd
Xavier Emma <xremma@protonmail.com>
2025-02-03 17:18:57 -0500

    Updated pop.js History and local storage

--- popup.js
+++ popup.js
@@ -34,14 +34,79 @@ function preventRefresh(e){
 
   */
 
-function saveHistory(searchRequest) {
-  let pastSearches = {};
-  pastSearches.history1 = searchRequest;
+async function saveHistory(searchRequest) {
+  //Pull history first
+  // History Queue is an aray of Strings with most recent searches
+  let historyQueue = [];
+  
+  // Fetch data from chrome
+  const data = await chrome.storage.sync.get("pastSearches");
+  if(data == undefined || data.pastSearches == undefined){
+    history.innerHTML = "It looks like you don't have any history yet. Try searching to see your past searches here!";
+  } else {
+      // For loop populating array
+    for(let i = 0; i < numHistorySearches; i++){
+      // Checks to make sure object pastSearches has the property we are looking for
+      if(Object.hasOwn(data.pastSearches, "history" + (i).toString())){
+        historyQueue[i] = data.pastSearches["history" + (i).toString()];
+      } 
+    }
+  }
 
+  //Actually Save the History
+  let counter = historyQueue.length;
+  if(counter >= numHistorySearches){
+    // shift everything down
+    for(let i = 0; i < numHistorySearches - 1; i++){
+      historyQueue[i] = historyQueue[i+1];
+    }
+    // Replace last element
+    historyQueue[counter - 1] = searchRequest;
+      
+  } else {
+    //Add another element to search history
+    historyQueue[counter] = searchRequest;
+  }
+
+  //Store search queue inside of past searches
+  let pastSearches = {};
+  
+  for(let i = 0; i < historyQueue.length; i++) {
+    Object.defineProperty(pastSearches, "history" + (i).toString(), {
+      value: historyQueue[i], writable: true, enumerable: true, configurable: true
+    });
+  }
+
+  //Tell chrome to store the data in past searches
   chrome.storage.sync.set({pastSearches});
 }
 
+// Function called to display history
 async function loadHistory() {
+  
+  // History Queue is an aray of Strings with most recent searches
+  let historyQueue = [];
+  
+  // Fetch data from chrome
+  const data = await chrome.storage.sync.get("pastSearches");
+  if(data == undefined || data.pastSearches == undefined){
+    history.innerHTML = "It looks like you don't have any history yet. Try searching to see your past searches here!";
+    return;
+  }
+
+  // For loop populating array
+  for(let i = numHistorySearches - 1; i >= 0; i--){
+    // Checks to make sure object pastSearches has the property we are looking for
+    if(Object.hasOwn(data.pastSearches, "history" + (i).toString())){
+      historyQueue[i] = data.pastSearches["history" + (i).toString()];
+
+      //This will be removed later
+      history.innerHTML += "\n" + (i).toString() + ":- " + historyQueue[i];
+    } 
+  }
+}
+ /*
+  // Fetch data from Chrome
   const data = await chrome.storage.sync.get("pastSearches");
   if(data != undefined && data.pastSearches != undefined && "history1" in data.pastSearches) {
     const pastSearches = {};
@@ -51,6 +116,7 @@ async function loadHistory() {
     history.innerHTML = "It looks like you don't have any history yet. Try searching to see your past searches here!";
   }
 }
+*/
 
 function processSearchBox(e){
   // Saving a duplicate the user enters and reset original to empty
@@ -71,6 +137,9 @@ let searchBoxElement = document.getElementById("searchBar");
 
 // Runs the function processSearchBox
 searchBoxForm.addEventListener("submit",processSearchBox);
+
+// Starting Backend for History
+var numHistorySearches = 4;
 
 
 
